fba460359404d3c97bc2b4ad5538e8d4609b73e3
Eric Martinet <93914292+eric-martinet@users.noreply.github.com>
2022-03-05 17:32:39 +0100

    Update indicator_compute.sql

--- SQL/indicator_compute.sql
+++ SQL/indicator_compute.sql
@@ -1,5 +1,4 @@
 # Poverty line is half of median disposable income, here converted in PPP
-
 DROP TABLE IF EXISTS _PovertyLine;
 CREATE TABLE IF NOT EXISTS _PovertyLine
 SELECT PPP.C_ISO3 AS C_ISO3, PPP.Year AS Year, (MDI.Value / PPP.Value) / 2 AS Value
@@ -8,10 +7,10 @@ INNER JOIN OECD_MDI MDI
 ON PPP.C_ISO3 = MDI.C_ISO3 AND PPP.Year = MDI.Year
 ;
 
-
+# Table with all indicators and computation of composite indicator
 DROP TABLE IF EXISTS _CompositeIndicator;
 CREATE TABLE IF NOT EXISTS _CompositeIndicator
-SELECT C.Name, Pop.Year, Pop.Value AS Population, GDP.Value AS GDP, PGap.Value AS PovertyGap, ROUND(PLine.Value, 0) AS PovertyLine, ROUND((PGap.Value * PLine.Value * Pop.Value) / 1000000, 0) AS CostClosingRPoverty, ROUND(((PGap.Value * PLine.Value * Pop.Value) / 1000000) / GDP.Value, 3) AS CostClosingRPovertyPercGDP
+SELECT C.ISO3 AS CountryISO3, C.Name AS CountryName, Pop.Year AS Year, Pop.Value AS Population, GDP.Value AS GDP, PGap.Value AS PovertyGap, ROUND(PLine.Value, 0) AS PovertyLine, ROUND((PGap.Value * PLine.Value * Pop.Value) / 1000000, 0) AS CostClosingRPoverty, ROUND(((PGap.Value * PLine.Value * Pop.Value) / 1000000) / GDP.Value, 3) AS CostClosingRPovertyPercGDP
 FROM WB_Countries C
 INNER JOIN WB_Population Pop
 ON C.ISO3 = Pop.C_ISO3
