bae8dd88e391b5bb5caec9d4b5c47c083c05d2b3
Eric Martinet <93914292+eric-martinet@users.noreply.github.com>
2022-03-06 13:13:54 +0100

    indicator update

---
+++ SQL/indicator_analyse.sql
@@ -0,0 +1,37 @@
+# Determine latest available year by country
+CREATE TEMPORARY TABLE _CountryLatestAvailableYear
+SELECT CountryName, MAX(Year) as LatestAvailableYear
+FROM _CompositeIndicator
+GROUP BY CountryName;
+
+SELECT * FROM _CountryLatestAvailableYear;
+
+# Display all indicators for all countries for latest available year, sorted by alphabetical order
+SELECT CI.CountryName, CI.IncomeLevel, CLAY.LatestAvailableYear, CI.CostClosingRPovertyPercGDP, CI.CostClosingRPoverty, CI.Population, CI.GDP, CI.PovertyGap, CI.PovertyLine
+FROM _CompositeIndicator CI
+INNER JOIN _CountryLatestAvailableYear CLAY
+ON CI.CountryName = CLAY.CountryName AND CI.Year = CLAY.LatestAvailableYear
+ORDER BY CI.CountryName;
+
+# Display all indicators for top 5 countries in Cost of Closing Relative Poverty as % of GDP (top = less) for latest available year
+SELECT CI.CountryName, CI.IncomeLevel, CLAY.LatestAvailableYear, CI.CostClosingRPovertyPercGDP, CI.CostClosingRPoverty, CI.Population, CI.GDP, CI.PovertyGap, CI.PovertyLine, CI.PovertyLinePercGDPCap
+FROM _CompositeIndicator CI
+INNER JOIN _CountryLatestAvailableYear CLAY
+ON CI.CountryName = CLAY.CountryName AND CI.Year = CLAY.LatestAvailableYear
+ORDER BY CI.CostClosingRPovertyPercGDP ASC
+LIMIT 5;
+# Ireland is the country for which the cost-to-GDP of closing relative poverty is the lowest (3.2%).
+# The top 5 countries tend to have a poverty line relatively high compared to the GDP per capita (around 20% or below).
+# It denotes relative equality among the general population as the poverty line is half the media disposable income.
+
+# Display all indicators for bottom 5 countries in Cost of Closing Relative Poverty as % of GDP (bottom = greater) for latest available year
+SELECT CI.CountryName, CI.IncomeLevel, CLAY.LatestAvailableYear, CI.CostClosingRPovertyPercGDP, CI.CostClosingRPoverty, CI.Population, CI.GDP, CI.PovertyGap, CI.PovertyLine, CI.PovertyLinePercGDPCap
+FROM _CompositeIndicator CI
+INNER JOIN _CountryLatestAvailableYear CLAY
+ON CI.CountryName = CLAY.CountryName AND CI.Year = CLAY.LatestAvailableYear
+ORDER BY CI.CostClosingRPovertyPercGDP DESC
+LIMIT 5;
+# USA is the country for which the cost-to-GDP of closing relative poverty is the highest (12.1%)
+# Due to the size of US economy, it is equal to the GDP of Italy in absolute value!
+# The bottom 5 countries tend to have a poverty line relatively high compared to the GDP per capita (around 30% or more).
+# It denotes relative inequality among the general populationas as the poverty line is half the media disposable income.
--- SQL/indicator_compute.sql
+++ SQL/indicator_compute.sql
@@ -10,7 +10,7 @@ ON PPP.C_ISO3 = MDI.C_ISO3 AND PPP.Year = MDI.Year
 # Table with all indicators and computation of composite indicator
 DROP TABLE IF EXISTS _CompositeIndicator;
 CREATE TABLE IF NOT EXISTS _CompositeIndicator
-SELECT C.ISO3 AS CountryISO3, C.Name AS CountryName, Pop.Year AS Year, Pop.Value AS Population, GDP.Value AS GDP, PGap.Value AS PovertyGap, ROUND(PLine.Value, 0) AS PovertyLine, ROUND((PGap.Value * PLine.Value * Pop.Value) / 1000000, 0) AS CostClosingRPoverty, ROUND(((PGap.Value * PLine.Value * Pop.Value) / 1000000) / GDP.Value, 3) AS CostClosingRPovertyPercGDP
+SELECT C.ISO3 AS CountryISO3, C.Name AS CountryName, IC.Name AS IncomeLevel, Pop.Year AS Year, Pop.Value AS Population, GDP.Value AS GDP, PGap.Value AS PovertyGap, ROUND(PLine.Value, 0) AS PovertyLine, Round(PLine.Value / ( GDP.Value / (Pop.Value / 1000000)), 2) AS PovertyLinePercGDPCap, ROUND((PGap.Value * PLine.Value * Pop.Value) / 1000000, 0) AS CostClosingRPoverty, ROUND(((PGap.Value * PLine.Value * Pop.Value) / 1000000) / GDP.Value, 3) AS CostClosingRPovertyPercGDP
 FROM WB_Countries C
 INNER JOIN WB_Population Pop
 ON C.ISO3 = Pop.C_ISO3
@@ -20,4 +20,6 @@ INNER JOIN OECD_PovertyGap PGap
 ON C.ISO3 = PGap.C_ISO3 AND Pop.Year = PGap.Year
 INNER JOIN _PovertyLine PLine
 ON C.ISO3 = PLine.C_ISO3 AND Pop.Year = PLine.Year
+INNER JOIN WB_IncomeLevels IC
+ON C.WB_INCLEVEL_ID = IC.WB_INCLEVEL_ID
 ;
